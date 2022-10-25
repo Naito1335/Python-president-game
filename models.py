fb2d2cfd425320c11dd57fff8e247b70f5eb04c7
Naito1335 <tessier.maxime35@gmail.com>
2022-10-25 18:00:42 +0200

    Add Card and Deck

--- models.py
+++ models.py
@@ -1,4 +1,12 @@
 class Card:
-    pass
+    card_value = {'3':0, '4':1, '5':2, '6':3, '7':4, '8':5, '9':6, '10':7, 'V':8, 'D':9, 'R':10, 'A':11, '2':12 }
+    card = (2,3,4,5,6,7,8,9,10,'V','D','R','A')
+    color = ('♤','♡','T','C')
 
-#Test GitHub Desktop
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for color in Card.color :
+            for card in Card.card :
+                card = Card(card, color)
+                self.cards.append(card)
